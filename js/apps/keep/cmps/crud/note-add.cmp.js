cffb3f7ea6042a6cb65c23318c618a9ff4e1b345
RotemAs <rotemaslan1@gmail.com>
2022-03-03 09:56:13 +0200

    note add notes

--- js/apps/keep/cmps/crud/note-add.cmp.js
+++ js/apps/keep/cmps/crud/note-add.cmp.js
@@ -3,8 +3,20 @@ export default {
     props: ["noteTypes"],
     template: `
     <section class="note-add">
-
+    <input :type="fieldType" autocomplete="off" v-model="userData"
+	    :placeholder="placeholder" @keyup.enter="addNote" ref="newNoteEl" />
 
     </section>   
     `,
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+        
+    },
+    methods: {
+        
+    }
 };
